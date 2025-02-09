752f11f8d73594610a2a62bf8bd0c0d35c394590
Hibernator <gibernator@gmail.com>
2025-02-09 16:39:06 +0100

    Day 19 part 2

--- src/main/scala/ch/hibernator/adventofcode/day19/Day19.scala
+++ src/main/scala/ch/hibernator/adventofcode/day19/Day19.scala
@@ -12,6 +12,8 @@ object Day19 extends SolutionBaseSimple:
     val allPatterns = input.head.split(", ").sortBy(_.length).reverse
     val designs = input.drop(2)
 
+    // The method finds one arrangement but is unsuitable for finding all of them
+    // because both runtime and memory complexity is too high
     @tailrec
     def createDesign(stateStack: mutable.Stack[State], deadEnds: mutable.Set[String]): Seq[String] =
       // nothing else to try, impossible to create the design
@@ -20,9 +22,9 @@ object Day19 extends SolutionBaseSimple:
         val currentState = stateStack.pop()
         // design created successfully
         if currentState.remainingDesign.isEmpty then currentState.usedPatterns
-        // dead end, can backtrack right away
+        // dead end encountered before, can backtrack right away
         else if deadEnds.contains(currentState.remainingDesign) then createDesign(stateStack, deadEnds)
-        // dead end, have to backtrack
+        // new dead end, add to the list of know dead-ends and backtrack
         else if currentState.availablePatterns.isEmpty then
           createDesign(stateStack, deadEnds.addOne(currentState.remainingDesign))
         else
@@ -46,18 +48,33 @@ object Day19 extends SolutionBaseSimple:
             stateStack.push(currentState.copy(availablePatterns = currentState.availablePatterns.tail))
             createDesign(stateStack, deadEnds)
 
-    val result1 =
-      designs
-        .map { design =>
-          println(s"Processing design $design")
-          val patterns = createDesign(mutable.Stack(State(design, allPatterns, Seq[String]())), mutable.Set())
-          println(s"Patterns: ${patterns.mkString(",")}")
-          patterns
-        }
-        .count(_.nonEmpty)
+    val result1 = designs
+      .map(design => createDesign(mutable.Stack(State(design, allPatterns, Seq[String]())), mutable.Set()))
+      .count(_.nonEmpty)
 
-    println(result1)
+    // Finds number of all arrangements but not the actual arrangements
+    def findNumAllArrangements(design: String): Long =
+      val designLength = design.length
+      // key is the length from the end of the design
+      // value is the number of arrangements for sub-design
+      val designToNumArrangements = mutable.Map[Int, Long]().withDefaultValue(0L)
 
-    (result1, 1L)
+      // iterate through all sub-designs from end
+      for i <- 1 to design.length
+      do
+        val toDrop = designLength - i
+        val subDesign = design.drop(toDrop)
+        val matchingPatterns = allPatterns.filter(subDesign.startsWith)
+        val numAllNewArrangements = matchingPatterns.map { pattern =>
+          if pattern.length == i then 1 // the pattern is at the end of arrangement
+          else designToNumArrangements(i - pattern.length)
+        }.sum
+        designToNumArrangements.addOne(i, numAllNewArrangements)
+
+      designToNumArrangements(designLength)
+
+    val result2 = designs.map(findNumAllArrangements).sum
+
+    (result1, result2)
 
 case class State(remainingDesign: String, availablePatterns: Seq[String], usedPatterns: Seq[String])
